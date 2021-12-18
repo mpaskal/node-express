68e144af44e383f764e9fb5c7d6dd0ba4ff777fe
Marina Paskal <paskalm@rogers.com>
2021-12-18 12:03:14 -0500

    Add routers for partners and promotions

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const partnerRouter = require("./routes/partnerRouter");
 
 const hostname = "localhost";
 const port = 3005;
@@ -10,10 +12,12 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/campsites", campsiteRouter);
+app.use("/promotions", promotionRouter);
+app.use("/partners", partnerRouter);
 
 app.use(express.static(__dirname + "/public"));
 
-app.set("port", process.env.PORT || 3005);
+//app.set("port", process.env.PORT || 3005);
 app.use((req, res) => {
   console.log(req.headers);
   res.statusCode = 200;
